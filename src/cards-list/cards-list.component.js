938dbadfd1293fca1cfa5e26bcec0e3a64de6aca
Kai Bortscheller <kbortscheller@avarteq.de>
2017-10-06 09:01:48 +0200

    Add Redirect to Login if not loggedIn

--- src/cards-list/cards-list.component.js
+++ src/cards-list/cards-list.component.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import _ from 'lodash';
+import { Card, Col, message, Row, Select, Pagination } from 'antd';
 import { connect } from 'react-redux';
-import { Card, Col, Row, Select, Pagination } from 'antd';
+import { push } from 'react-router-redux';
 import SearchField from './search-field/search-field.component';
 import FilterColorField from './componenten/color-filter-field';
 import TypeFilterField from './componenten/type-filter-field';
@@ -18,13 +20,14 @@ const Option = Select.Option;
 const mapStateToProps = state => (
   {
     cards: state.cardsListReducer.cards,
-    singleCard: state.cardsListReducer.card,
     cardId: state.cardsListReducer.cardId,
-    loading: state.cardsListReducer.isLoading,
     cardLoading: state.cardsListReducer.isCardLoading,
+    isLoggedIn: _.size(state.loginReducer.isLoggedIn) > 0 ? state.loginReducer.isLoggedIn : JSON.parse(localStorage.getItem('isLoggedIn')),
+    loading: state.cardsListReducer.isLoading,
     params: state.cardsListReducer.params,
     searching: state.cardsListReducer.isSearching,
     searchResult: state.cardsListReducer.searchCardsResult,
+    singleCard: state.cardsListReducer.card,
     totalCount: state.cardsListReducer.totalCount,
     types: state.cardsListReducer.types,
     visible: state.cardsListReducer.visible,
@@ -55,6 +58,11 @@ const handleFilterByType = (type, dispatch, currentParams) => {
   return dispatch(fetchCardsRequest(params));
 };
 
+const handleRedirect = (errorMessage, path, dispatch) => {
+  message.error(errorMessage);
+  dispatch(push(path));
+};
+
 const mapDispatchToProps = dispatch => ({
   loadCards: params => (dispatch(fetchCardsRequest(params))),
   loadCard: cardId => (dispatch(fetchCardRequest(cardId))),
@@ -65,6 +73,7 @@ const mapDispatchToProps = dispatch => ({
   },
   onFilterByColor: (colors, params) => { handleFilterByColor(colors, dispatch, params); },
   onFilterByType: (type, params) => { handleFilterByType(type, dispatch, params); },
+  onRedirect: () => handleRedirect('Sie haben keine Berechtigung für diese Seite.', '/', dispatch),
   onSearch: (params, keyword) => { handleSearch(params, dispatch, keyword); },
   onShowModal: cardId => dispatch(showModal(cardId)),
   onHideModal: () => dispatch(hideModal()),
@@ -88,11 +97,13 @@ class CardsList extends React.Component {
       singleCard,
       cardId,
       cardLoading,
+      isLoggedIn,
       loadCard,
       onChangePage,
       onChangePageSize,
       onFilterByColor,
       onFilterByType,
+      onRedirect,
       onShowModal,
       onHideModal,
       params,
@@ -105,68 +116,76 @@ class CardsList extends React.Component {
 
     return (
       <div>
-        <Row>
-          <Col span={6}>
-            <SearchField
-              onSearchCards={onSearch}
-              params={params}
-            />
-          </Col>
-          <Col span={6}>
-            <FilterColorField
-              onFilterByColor={onFilterByColor}
-              params={params}
-            />
-          </Col>
-          <Col span={6}>
-            <TypeFilterField
-              onFilterByType={onFilterByType}
-            >
+        { isLoggedIn ?
+          <div>
+            <Row>
+              <Col span={6}>
+                <SearchField
+                  onSearchCards={onSearch}
+                  params={params}
+                />
+              </Col>
+              <Col span={6}>
+                <FilterColorField
+                  onFilterByColor={onFilterByColor}
+                  params={params}
+                />
+              </Col>
+              <Col span={6}>
+                <TypeFilterField
+                  onFilterByType={onFilterByType}
+                  params={params}
+                >
+                  {
+                    types.map(type => <Option key={type}>{type}</Option>)
+                  }
+                </TypeFilterField>
+              </Col>
+            </Row>
+            <br />
+            <Row>
               {
-                types.map(type => <Option key={type}>{type}</Option>)
+                cards.map(card => (
+                  <Col xs={13} sm={10} md={7} lg={5} xl={4} key={card.multiverseid}>
+                    <Card
+                      style={{ width: 240 }}
+                      bodyStyle={{ padding: 0 }}
+                      key={card.id}
+                      loading={loading}
+                      onClick={() => onShowModal(card.multiverseid)}
+                    >
+                      <div className="custom-image">
+                        <img alt={card.name} width="100%" src={card.imageUrl} />
+                      </div>
+                    </Card>
+                  </Col>
+                ))
               }
-            </TypeFilterField>
-          </Col>
-        </Row>
-        <br />
-        <Row>
-          {
-            cards.map(card => (<Col xs={13} sm={10} md={7} lg={5} xl={4} key={card.multiverseid}>
-              <Card
-                style={{ width: 240 }}
-                bodyStyle={{ padding: 0 }}
-                key={card.id}
-                loading={loading}
-                onClick={() => onShowModal(card.multiverseid)}
-              >
-                <div className="custom-image">
-                  <img alt={card.name} width="100%" src={card.imageUrl} />
-                </div>
-              </Card>
-            </Col>))
-          }
-        </Row>
-        <Row>
-          <Col>
-            <Pagination
-              showSizeChanger
-              onShowSizeChange={(page, pageSize) => onChangePageSize(params, page, pageSize)}
-              onChange={(page, pageSize) => onChangePage(params, page, pageSize)}
-              defaultCurrent={params.page}
-              total={Number(totalCount)}
-              defaultPageSize={params.pageSize}
-              pageSizeOptions={['25', '50', '75', '100']}
+            </Row>
+            <Row>
+              <Col>
+                <Pagination
+                  showSizeChanger
+                  onShowSizeChange={(page, pageSize) => onChangePageSize(params, page, pageSize)}
+                  onChange={(page, pageSize) => onChangePage(params, page, pageSize)}
+                  defaultCurrent={params.page}
+                  total={Number(totalCount)}
+                  defaultPageSize={params.pageSize}
+                  pageSizeOptions={['25', '50', '75', '100']}
+                />
+              </Col>
+            </Row>
+            <DetailModal
+              cardId={cardId}
+              card={singleCard}
+              visible={visible}
+              onHideModal={onHideModal}
+              loadCard={loadCard}
+              loading={cardLoading}
             />
-          </Col>
-        </Row>
-        <DetailModal
-          cardId={cardId}
-          card={singleCard}
-          visible={visible}
-          onHideModal={onHideModal}
-          loadCard={loadCard}
-          loading={cardLoading}
-        />
+          </div> :
+          onRedirect()
+        }
       </div>
     );
   }
